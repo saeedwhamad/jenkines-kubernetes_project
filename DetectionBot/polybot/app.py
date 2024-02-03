fdf8ef7dd75fae76e2314aa20a7db0e2f2f8f8ab
saeedwhamad <saeedhamad97@hotmail.com>
2024-02-03 16:23:32 +0200

    add threads

--- DetectionBot/polybot/app.py
+++ DetectionBot/polybot/app.py
@@ -148,3 +148,18 @@ def load_test():
 if __name__ == "__main__":
     bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
     app.run(host='0.0.0.0', port=8443)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
